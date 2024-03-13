70a9b5cd217bd49903b91dd7bc318fb33016ce36
Luiz Otávio <luizotavioautomacao@gmail.com>
2024-03-13 17:41:11 -0300

    refactor: implements IAuthenticaion

--- src/data/usecases/authentication/remote-authentication.ts
+++ src/data/usecases/authentication/remote-authentication.ts
@@ -1,15 +1,16 @@
-import { AuthenticationParams } from "../../../domain/usecases/authentication";
+import { AuthenticationParams, IAuthentication } from "../../../domain/usecases/authentication";
 import { HttpPostClient } from "../../../data/protocols/http/http-post-client";
 import { HttpStatusCode } from "../../../data/protocols/http/http-response";
 import { InvalidCredentialsError } from "../../../domain/errors/invalid-credencials-error";
 import { UnexpectedError } from "../../../domain/errors/unexpected-error";
 import { AccountModel } from "../../../domain/model/account-model";
 
-export class RemoteAuthentication {
+export class RemoteAuthentication implements IAuthentication {
     constructor(
         private readonly url: string,
         private readonly httpPostClient: HttpPostClient<AuthenticationParams, AccountModel>
     ) { }
+
     async auth(params: AuthenticationParams): Promise<AccountModel> {
         const htppResponse = await this.httpPostClient.post({ url: this.url, body: params })
 
