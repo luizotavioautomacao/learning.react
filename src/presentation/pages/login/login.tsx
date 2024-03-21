fa41cbfb1b5fa0eb093d578bd58a290de8c9bc71
Luiz Otávio <luizotavioautomacao@gmail.com>
2024-03-21 01:10:12 -0300

    refactor: move form status to a separate file

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import Styles from "./login-styles.scss";
-import Spinner from "@/presentation/components/spinner/spinner";
 import Header from "@/presentation/components/header/header";
 import Footer from "@/presentation/components/footer/footer";
+import Input from "@/presentation/components/input/input";
+import FormStatus from "@/presentation/components/form-status/form-status";
 
 const Login: React.FC = () => {
   return (
@@ -10,26 +11,11 @@ const Login: React.FC = () => {
       <Header />
       <form className={Styles.form}>
         <h2>Login</h2>
-        <div className={Styles.inputWrap}>
-          <input type="email" name="email" placeholder="Digite seu e-mail" />
-          <span className={Styles.status}>[x]</span>
-        </div>
-        <div className={Styles.inputWrap}>
-          <input
-            type="password"
-            name="password"
-            placeholder="Digite sua senha"
-          />
-          <span className={Styles.status}>[x]</span>
-        </div>
-        <button className={Styles.submit} type="submit">
-          Entrar
-        </button>
+        <Input type="email" name="email" placeholder="Digite seu e-mail" />
+        <Input type="password" name="password" placeholder="Digite sua senha" />
+        <button className={Styles.submit} type="submit">Entrar</button>
         <span className={Styles.link}>Criar conta</span>
-        <div className={Styles.errorWrap}>
-          <Spinner className={Styles.spinner} />
-          <span className={Styles.error}>Erro</span>
-        </div>
+        <FormStatus />
       </form>
       <Footer />
     </div>
