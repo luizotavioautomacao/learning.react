b8573065a9ff44340afc3771e794de94feba44f6
Luiz Otávio <luizotavioautomacao@gmail.com>
2024-05-04 02:36:38 -0300

    feat: router Appx

--- src/presentation/components/router/router.tsx
+++ src/presentation/components/router/router.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { Login } from "@/presentation/pages";
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import '@/presentation/styles/global.scss';
 
 const Spinner: React.FC = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/login" Component={Login}/>
+        <Route path="/login" Component={Login} />
+        <Route path='/' element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
