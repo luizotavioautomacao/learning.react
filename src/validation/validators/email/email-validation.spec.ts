f29e0ef72c867a8ba3dd8ba5b460205be9d7eff1
luizotavioautomacao <luizotavioautomacao@gmail.com>
2025-05-03 00:22:19 -0300

    test: ensure EmailValidation returns falsy if email is valid

--- src/validation/validators/email/email-validation.spec.ts
+++ src/validation/validators/email/email-validation.spec.ts
@@ -2,20 +2,22 @@ import { InvalidFieldError } from '@/validation/errors/invalid-field-error';
 import { faker } from '@faker-js/faker';
 import { EmailValidation } from '@/validation/validators/email/email-validation';
 
-const makeSut = (): EmailValidation => new EmailValidation(faker.string.uuid());
+const emailInvalid = faker.string.uuid();
+
+const makeSut = (): EmailValidation => new EmailValidation(faker.database.column());
 
 describe("EmailValidation", () => {
 
     test("Should return error if email is invalid", () => {
         const sut = makeSut();
-        const error = sut.validate('');
+        const error = sut.validate(emailInvalid);
         expect(error).toEqual(new InvalidFieldError())
     });
 
-    // test("Should return false if field is field not empty", () => {
-    //     const sut = makeSut();
-    //     const error = sut.validate(faker.string.uuid());
-    //     expect(error).toBeFalsy();
-    // });
+    test("Should return false if field is email is valid", () => {
+        const sut = makeSut();
+        const error = sut.validate(faker.internet.email());
+        expect(error).toBeFalsy();
+    });
 
 });
