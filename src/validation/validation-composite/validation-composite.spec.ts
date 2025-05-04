cfd9b5adf8871a77b88e7c762f8e278da27689e0
luizotavioautomacao <luizotavioautomacao@gmail.com>
2025-05-03 23:35:49 -0300

    test: ensure ValidationComposite returns error if any validation fails

---
+++ src/validation/validation-composite/validation-composite.spec.ts
@@ -0,0 +1,36 @@
+import { ValidationComposite } from '@/validation/validation-composite/validation-composite';
+import { FieldValidationSpy } from '@/validation/validators/test/mock-field-validation';
+import { faker } from '@faker-js/faker';
+
+const makeSut = (): ValidationComposite => {
+    const fieldValidationSpy = new FieldValidationSpy('any_field');
+    const fieldValidationSpy2 = new FieldValidationSpy('any_field');
+    fieldValidationSpy2.error = new Error('any_error_message');
+    
+    return new ValidationComposite([
+        fieldValidationSpy,
+        fieldValidationSpy2
+    ])
+}
+
+describe("ValidationComposite", () => {
+
+    test("Should return error if any validation fails", () => {
+        const sut = makeSut();
+        const error = sut.validate('any_field', 'any_value');
+        expect(error).toBe('any_error_message');
+    });
+
+    // test("Should return false if field is valid email", () => {
+    //     const sut = makeSut();
+    //     const error = sut.validate(faker.internet.email());
+    //     expect(error).toBeFalsy();
+    // });
+
+    // test("Should return false if field is email empty", () => {
+    //     const sut = makeSut();
+    //     const error = sut.validate(faker.internet.email());
+    //     expect(error).toBeFalsy();
+    // });
+
+});
--- src/validation/validators/email/email-validation.spec.ts
+++ src/validation/validators/email/email-validation.spec.ts
@@ -26,5 +26,4 @@ describe("EmailValidation", () => {
         expect(error).toBeFalsy();
     });
 
-
 });
