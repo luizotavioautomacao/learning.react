e59135b7fec1805e7e85bf6a6436fbff9f8b5ba8
luizotavioautomacao <luizotavioautomacao@gmail.com>
2025-05-03 23:50:35 -0300

    refactor: create makeSut

--- src/validation/validation-composite/validation-composite.spec.ts
+++ src/validation/validation-composite/validation-composite.spec.ts
@@ -1,37 +1,33 @@
 import { ValidationComposite } from '@/validation/validation-composite/validation-composite';
 import { FieldValidationSpy } from '@/validation/validators/test/mock-field-validation';
-import { faker } from '@faker-js/faker';
 
-const makeSut = (): ValidationComposite => {
-    const fieldValidationSpy = new FieldValidationSpy('any_field');
-    const fieldValidationSpy2 = new FieldValidationSpy('any_field');
-    fieldValidationSpy.error = new Error('any_first_error_message');
-    fieldValidationSpy2.error = new Error('any_second_error_message');
-    
-    return new ValidationComposite([
-        fieldValidationSpy,
-        fieldValidationSpy2
-    ])
+type SutTypes = {
+    sut: ValidationComposite,
+    fieldValidationsSpy: FieldValidationSpy[]
+}
+
+const makeSut = (): SutTypes => {
+    const fieldValidationsSpy = [
+        new FieldValidationSpy('any_field'),
+        new FieldValidationSpy('any_field')
+    ]
+
+    const sut = new ValidationComposite(fieldValidationsSpy)
+
+    return {
+        sut,
+        fieldValidationsSpy
+    }
 }
 
 describe("ValidationComposite", () => {
 
     test("Should return error if any validation fails", () => {
-        const sut = makeSut();
+        const { sut, fieldValidationsSpy } = makeSut();
+        fieldValidationsSpy[0].error = new Error('any_first_error_message');
+        fieldValidationsSpy[1].error = new Error('any_second_error_message');
         const error = sut.validate('any_field', 'any_value');
         expect(error).toBe('any_first_error_message');
     });
-
-    // test("Should return false if field is valid email", () => {
-    //     const sut = makeSut();
-    //     const error = sut.validate(faker.internet.email());
-    //     expect(error).toBeFalsy();
-    // });
-
-    // test("Should return false if field is email empty", () => {
-    //     const sut = makeSut();
-    //     const error = sut.validate(faker.internet.email());
-    //     expect(error).toBeFalsy();
-    // });
 
 });
