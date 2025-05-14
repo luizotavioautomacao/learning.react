6d94d87cf16ac546ca0b2c99dcf48a1e825446a4
luizotavioautomacao <luizotavioautomacao@gmail.com>
2025-05-14 15:27:55 -0300

    test: ensure ValidationBuilder has a required helper

---
+++ src/validation/validators/builder/validation-builder.spec.ts
@@ -0,0 +1,11 @@
+import { RequiredFieldValidation } from '../required-field/required-field-validation';
+import { ValidationBuilder } from '@/validation/validators/builder/validation-builder';
+
+describe("ValidationBuilder", () => {
+
+    test("Should return RequiredFieldValidation", () => {
+        const validations = ValidationBuilder.field('any_field').required().build();
+        expect(validations).toEqual([new RequiredFieldValidation('any_field')]);
+    });
+
+});
